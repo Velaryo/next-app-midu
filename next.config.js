fb0777a120a8d6c64e6fd708740610c503e97dcf
Alvaro <ixml3d@gmail.com>
2023-02-26 16:52:14 -0500

    instalación

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // para permitir uso de rutos
+  // nueva forma
+  experimental: {
+    appDir: true
+  },
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
