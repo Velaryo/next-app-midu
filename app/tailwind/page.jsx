a62d9a352824401320345581d649d504c44ac854
Alvaro <ixml3d@gmail.com>
2023-03-11 15:44:01 -0500

    tw

---
+++ app/tailwind/page.jsx
@@ -0,0 +1,22 @@
+'use client'
+
+import Link from "next/link"
+
+
+
+
+
+export default function App() {
+  return (
+    <Link
+  color="secondary"
+  href="#"
+  isBlock
+  showAnchorIcon
+  size="md"
+>
+  "First solve the problem. Then, write the code." - Jon Johnson.
+</Link>
+  )
+}
+
